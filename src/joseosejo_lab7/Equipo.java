b14c474539e07cd88a78d1124b27a6123b670448
Jose Carlos Osejo <jcoq2003@gmail.com>
2022-03-04 13:52:32 -0600

    Clases tabla posiciones

--- src/joseosejo_lab7/Equipo.java
+++ src/joseosejo_lab7/Equipo.java
@@ -9,13 +9,6 @@ package joseosejo_lab7;
  *
  * @author jcoq2
  */
-public class JoseOsejo_Lab7 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Equipo {
     
 }
